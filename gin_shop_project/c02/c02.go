8c3ccfd36870e2296aa9492246a48b708edce457
Kris shin <KrisShin5014@outlook.com>
2020-11-25 08:46:09 +0800

    ajax form gin

--- gin_shop_project/c02/c02.go
+++ gin_shop_project/c02/c02.go
@@ -1,6 +1,7 @@
 package c02
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -39,4 +40,43 @@ func ArrStruct(con *gin.Context) {
 	arr_struct := [3]UserInfo{{Id: 1, Name: "kris", Age: 12, Addr: "xxx"}, {Id: 2, Name: "kris2", Age: 13, Addr: "xxx2"}, {Id: 3, Name: "kris3", Age: 14, Addr: "xxx3"}}
 	con.HTML(http.StatusOK, "cha02/arr_struct.html", arr_struct)
 
+}
+
+func MapController(ctx *gin.Context) {
+	map_data := map[string]string{"name": "kris", "age": "18"}
+	map_data2 := map[string]int{"age": 19}
+	map_data3 := map[string]interface{}{"map_data": map_data, "map_data2": map_data2}
+	ctx.HTML(http.StatusOK, "cha02/map.html", map_data3)
+}
+
+func MapStruct(ctx *gin.Context) {
+	map_struct := map[string]UserInfo{"user": {Id: 1, Name: "kris", Age: 19, Addr: "xxxk"}}
+	ctx.HTML(http.StatusOK, "cha02/map_struct", map_struct)
+}
+
+func Param1(ctx *gin.Context) {
+	id := ctx.Param("id")
+	ctx.String(http.StatusOK, "hello %s", id)
+}
+func Param2(ctx *gin.Context) {
+	id := ctx.Param("id")
+	ctx.String(http.StatusOK, "hello %s", id)
+}
+func GetQueryData(ctx *gin.Context) {
+	id := ctx.Query("id")
+
+	name:=ctx.DefaultQuery("name","www")
+	fmt.Println(name)
+	ctx.String(http.StatusOK, "hello %s", id)
+}
+
+func ToUserAdd(ctx *gin.Context){
+	ctx.HTML(http.StatusOK,"cha02/user_add",nil)
+}
+
+func DoUserAdd(ctx *gin.Context){
+username:=ctx.PostForm("username")
+	password:=ctx.PostForm("password")
+	fmt.Println(username,password)
+	ctx.String(http.StatusOK,"success!!!")
 }
--- gin_shop_project/main.go
+++ gin_shop_project/main.go
@@ -17,6 +17,17 @@ func main() {
 	router.GET("/user_struct", c02.UserInfoStruct)
 	router.GET("/arr", c02.ArrController)
 	router.GET("/arr_struct", c02.ArrStruct)
+	router.GET("/map",c02.MapController)
+	router.GET("/map_struct",c02.MapStruct)
+
+	// 路由获取参数 强制匹配
+	router.GET("/param1/:id",c02.Param1)
+	// 路由匹配参数 非强制匹配
+	router.GET("/param2/*id",c02.Param2)
+
+	router.GET("/query",c02.GetQueryData)
+	router.GET("/to_user_add",c02.ToUserAdd)
+	router.GET("/do_user_add",c02.DoUserAdd)
 
 	router.Run(":8989")
 }
